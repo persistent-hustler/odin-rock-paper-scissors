93e0335d20ee1787783179fea2c0122ae9ce736f
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-05-07 19:35:42 +0530

    Create a function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice = (randomNumber === 0) ? 'rock' :
+    (randomNumber === 1) ? 'paper' : 'scissors';
+
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
