c3f8ea643b42a75064057a47e5e4bd7eb3f3b61d
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-05-14 21:45:41 +0530

    Update DOM elements after each game round

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+let choices = document.querySelector('.choices');
+let computerChoiceDisplay = document.querySelector('#computer-choice')
+let roundResultDisplay = document.querySelector('#round-result');
+let computerScoreDisplay = document.querySelector('#computer-score');
+let playerScoreDisplay = document.querySelector('#player-score');
+let playerScore = 0;
+let computerScore = 0;
 
+choices.addEventListener('click', (event) => {
+    let playerChoice = event.target;
+    let computerChoice = getComputerChoice();
+    computerChoiceDisplay.textContent = computerChoice;
+    
+    let roundResult = playRound(playerChoice.id, computerChoice);
+    roundResultDisplay.textContent = roundResult;
+
+    if (roundResult === 'win') {
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+    } else if(roundResult === 'lose') {
+        computerScore++;
+        computerScoreDisplay.textContent = computerScore;
+    }
+});
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -15,21 +38,21 @@ function playRound(humanChoice, computerChoice) {
             roundResult = (humanChoice === 'rock') ? 'tie' :
             (humanChoice === 'paper') ? 'win' :
             (humanChoice === 'scissors') ? 'lose' :
-            'invalid human choice';
+            'did not choose in';
             break;
         
         case 'paper' :
             roundResult = (humanChoice === 'rock') ? 'lose' :
             (humanChoice === 'paper') ? 'tie' :
             (humanChoice === 'scissors') ? 'win' :
-            'invalid human choice' ;
+            'did not choose in' ;
             break;
         
         case 'scissors' :
             roundResult = (humanChoice === 'rock') ? 'win' :
             (humanChoice === 'paper') ? 'lose' :
             (humanChoice === 'scissors') ? 'tie' :
-            'invalid human choice' ;
+            'did not choose in' ;
             break;
         
         default : roundResult = 'computer choice error';
